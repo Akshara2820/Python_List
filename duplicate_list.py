330ad703a8d84d9890d5c100eca447caefbdccf6
Akshara Mishra <72369600+Akshara2820@users.noreply.github.com>
2020-11-06 10:58:56 +0530

    Add files via upload

--- duplicate_list.py
+++ duplicate_list.py
@@ -4,14 +4,31 @@ l=len(number)
 i=0
 num=number
 Duplicates=[]
-k=[]
 while i<l:
     j=0
-    # k=[]
     while j<l:
         if num[i] == num[j]:
            if num[i]not in Duplicates:
                Duplicates.append(num[i])
         j+=1
+    i+=1
+print(Duplicates)
+
+
+
+
+
+number= [19, 17, 12, 17, 17, 18, 10, 17, 14, 12, 19, 17, 12, 13, 11]
+l=len(number)
+Duplicates = [] 
+i=0
+while i<l: 
+    j = i+1
+    while j<l:
+        if number[i] == number[j]:
+        	if number[i] not in Duplicates: 
+        		Duplicates.append(number[i])
+        j+=1
+
     i+=1
 print(Duplicates)
